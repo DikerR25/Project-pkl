bc11b5a1bc9c0fb8a1e9e655dc8342d6406d0b97
DikerR25 <93419907+DikerR25@users.noreply.github.com>
2023-09-18 21:38:45 +0700

    Update register.blade.php

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -111,9 +111,8 @@
 					<div class="d-flex flex-center flex-wrap fs-6 p-5 pb-0">
 						<!--begin::Links-->
 						<div class="d-flex flex-center fw-semibold fs-6">
-							<a href="https://keenthemes.com" class="text-muted text-hover-primary px-2" target="_blank">About</a>
-							<a href="https://devs.keenthemes.com" class="text-muted text-hover-primary px-2" target="_blank">Support</a>
-							<a href="https://keenthemes.com/products/saul-html-pro" class="text-muted text-hover-primary px-2" target="_blank">Purchase</a>
+							<a href="/about" class="text-muted text-hover-primary px-2" target="_blank">About</a>
+							<a href="/support" class="text-muted text-hover-primary px-2" target="_blank">Support</a>
 						</div>
 						<!--end::Links-->
 					</div>
