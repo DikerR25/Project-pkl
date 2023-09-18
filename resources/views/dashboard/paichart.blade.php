222562aa04131f56905c1a25cec6390dbb08dea9
rozaqms <142777047+rozaqms@users.noreply.github.com>
2023-09-18 14:47:06 +0700

    Update paichart.blade.php

--- resources/views/dashboard/paichart.blade.php
+++ resources/views/dashboard/paichart.blade.php
@@ -25,14 +25,14 @@
                 <div class="d-flex fs-6 fw-semibold align-items-center mb-1">
                     <div class="bullet me-3" style="background-color: #FF6384" ></div>
                     <div class="text-gray-400">Makanan</div>
-                    <div class="ms-auto fw-bold text-gray-700" id="makananCount">0</div>
+                    <div class="ms-auto fw-bold text-gray-700" id="lainnyaCount">0</div>
                 </div>
                 <!--end::Label-->
                 <!--begin::Label-->
                 <div class="d-flex fs-6 fw-semibold align-items-center mb-12">
                     <div class="bullet bg-warning me-3"></div>
                     <div class="text-gray-400">Lainnya</div>
-                    <div class="ms-auto fw-bold text-gray-700" id="lainnyaCount">0</div>
+                    <div class="ms-auto fw-bold text-gray-700" id="makananCount">0</div>
                 </div>
                 <!--end::Label-->
             </div>
