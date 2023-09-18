569b79d0b7bc32f58f6730984f6ef7b578b18a15
rozaqms <142777047+rozaqms@users.noreply.github.com>
2023-09-18 14:29:57 +0700

    Update linechart.blade.php

--- resources/views/dashboard/linechart.blade.php
+++ resources/views/dashboard/linechart.blade.php
@@ -1,11 +1,8 @@
 <div class="col-lg-12 col-xl-12 col-xxl-6 mb-5 mb-xl-0">
-    <div class="row gx-5 gx-xl-10">
-        <!--begin::Col-->
-        <div class="col-xxl-6 mb-5 mb-xl-10">
             <!--begin::Chart widget 8-->
-            <div class="card card-flush h-xl-100">
+            <div class="card card-flush overflow-hidden h-md-100">
                 <!--begin::Header-->
-                <div class="card-header pt-5">
+                <div class="card-header py-5">
                     <!--begin::Title-->
                     <h3 class="card-title align-items-start flex-column">
                         <span class="card-label fw-bold text-dark">Penghasilan Bersih</span>
@@ -37,12 +34,20 @@
                                 <!--begin::Statistics-->
                                 <div class="d-flex align-items-center mb-2">
                                     <span class="fs-4 fw-semibold text-gray-400 me-1">Rp</span>
-                                    <span class="fs-2hx fw-bold text-gray-800 me-2 lh-1 ls-n2" data-kt-countup="true" data-kt-countup-value="{{number_format($penjualanTerjual - $pengeluaranT,2)}}">0</span>
+                                    <span class="fs-2hx fw-bold text-gray-800 me-2 lh-1 ls-n2" data-kt-countup="true" data-kt-countup-value="{{number_format($pendapatanbersihbulanini,2)}}">0</span>
+                                    @if ($persentasePerubahanbulanini > -1) 
                                     <span class="badge badge-light-success fs-base">
-                                    <i class="ki-duotone ki-arrow-up fs-5 text-success ms-n1">
+                                        <i class="ki-duotone ki-arrow-up fs-5 text-success ms-n1">
                                         <span class="path1"></span>
                                         <span class="path2"></span>
-                                    </i>4,8%</span>
+                                        </i>{{ number_format($persentasePerubahanbulanini, 2) }}%</span> 
+                                @else 
+                                    <span class="badge badge-light-danger fs-base">
+                                        <i class="ki-duotone ki-arrow-down fs-5 text-danger ms-n1">
+                                            <span class="path1"></span>
+                                            <span class="path2"></span>
+                                        </i>{{ number_format($persentasePerubahanbulanini, 2) }}%</span> 
+                                @endif
                                 </div>
                                 <!--end::Statistics-->
                                 <!--begin::Description-->
@@ -94,9 +99,6 @@
                 <!--end::Body-->
             </div>
             <!--end::Chart widget 8-->
-        </div>
-        <!--end::Col-->
-    </div>
 </div>
 
     @push('kt_charts_penjualan')
